e1db7069bb5933074f60abf771b0be232e44e16a
Manuel Müller <mxman@gmx.net>
2016-04-06 17:40:25 +0200

    Changed JavaScript code to be OpenLayers v3.15 conform, Added layer selection panel, Added OSM bus stop retrieval, Added manual bus stop activity patterns and first steps for data-driven pattern extraction.

--- detect_bus_stops.py
+++ detect_bus_stops.py
@@ -1,7 +1,8 @@
 import copy
 import pygeoj
 import utm
-from  shapely.geometry import Point,LineString
+import overpass
+from  shapely.geometry import Point,LineString,MultiPolygon
 import charting
 
 DATA_PATH = 'data/'
@@ -65,6 +66,13 @@ class Route:
         for c in feature.geometry.coordinates:
             utm_coordinates.append(utm.from_latlon(c[1],c[0])[:2])  # cut off UTM zone
         self.geometry = LineString(utm_coordinates)
+
+
+class BusStop:
+    def __init__(self, coordinates):
+        utm_coordinates = utm.from_latlon(coordinates[1], coordinates[0])
+        self.geometry = Point(utm_coordinates[0], utm_coordinates[1])
+        self.nearby_activity_points = []
             
 
 class GeoJSONError(Exception):
@@ -120,36 +128,53 @@ def filter_by_distance(activity_points, routes):
                 break
     return filtered_activity_points
 
+def filter_by_activity_combinations(activity_points, combinations):
+    filtered_activity_points = {}
+    for (id, point) in activity_points.items():
+        for (previous_activity, current_activity) in combinations:
+            if point.previous_dominating_activity == previous_activity and point.current_dominating_activity == current_activity:
+                filtered_activity_points[id] = point
+                break
+    return filtered_activity_points
 
-# Recursive function to fill missing previous and current activities (where possible)   
-def enhance_activity_points(activity_points, recursion_step=0):
-    if recursion_step == 0:
+def check_activity_consistency(activity_points):
+    for (id,point) in activity_points.items():
+        if point.previous_dominating_activity is not None and id-1 in activity_points and activity_points[id-1].current_dominating_activity is not None:
+            if point.previous_dominating_activity != activity_points[id-1].current_dominating_activity:
+                return False
+        if point.current_dominating_activity is not None and id+1 in activity_points and activity_points[id+1].previous_dominating_activity is not None:
+            if point.current_dominating_activity != activity_points[id+1].previous_dominating_activity:
+                return False
+    return True
+
+
+# Function to fill missing previous and current activities (where possible)   
+def enhance_activity_points(activity_points):
+    if check_activity_consistency(activity_points):
         print 'Trying to fill missing activity labels:'
-    enhanced_previous_dominating_activities = 0
-    enhanced_current_dominating_activities = 0
-
-    for point in activity_points.values():
-        if point.previous_dominating_activity is None:
-            if (point.id-1 in activity_points and
-                activity_points[point.id-1].current_dominating_activity is not None):
-                    point.previous_dominating_activity = activity_points[point.id-1].current_dominating_activity
-                    point.previous_dominating_activity_confidence = activity_points[point.id-1].current_dominating_activity_confidence
-                    enhanced_previous_dominating_activities += 1
-        if point.current_dominating_activity is None:
-            if (point.id+1 in activity_points and
-                activity_points[point.id+1].previous_dominating_activity is not None):
-                    point.current_dominating_activity = activity_points[point.id+1].previous_dominating_activity
-                    point.current_dominating_activity_confidence = activity_points[point.id+1].previous_dominating_activity_confidence
-                    enhanced_current_dominating_activities += 1
-
-    print 'Recursion step: '+str(recursion_step)
-    print 'Added '+str(enhanced_previous_dominating_activities)+' previous_dominating_activities.'
-    print 'Added '+str(enhanced_current_dominating_activities)+' current_dominating_activities.'
-    if enhanced_previous_dominating_activities > 0 or enhanced_current_dominating_activities > 0:
-        return enhance_activity_points(activity_points,recursion_step+1)
+        enhanced_previous_dominating_activities = 0
+        enhanced_current_dominating_activities = 0
+    
+        for point in activity_points.values():
+            if point.previous_dominating_activity is None:
+                if (point.id-1 in activity_points and
+                    activity_points[point.id-1].current_dominating_activity is not None):
+                        point.previous_dominating_activity = activity_points[point.id-1].current_dominating_activity
+                        point.previous_dominating_activity_confidence = activity_points[point.id-1].current_dominating_activity_confidence
+                        enhanced_previous_dominating_activities += 1
+            if point.current_dominating_activity is None:
+                if (point.id+1 in activity_points and
+                    activity_points[point.id+1].previous_dominating_activity is not None):
+                        point.current_dominating_activity = activity_points[point.id+1].previous_dominating_activity
+                        point.current_dominating_activity_confidence = activity_points[point.id+1].previous_dominating_activity_confidence
+                        enhanced_current_dominating_activities += 1
+        print 'Added '+str(enhanced_previous_dominating_activities)+' previous_dominating_activities.'
+        print 'Added '+str(enhanced_current_dominating_activities)+' current_dominating_activities.'
     else:
-        return activity_points
+        print 'Previous/current activities not consistent with ids. No enhancement possible :('
+    return activity_points
 
+      
 
 def profile_activity_points(activity_points):
     original_points = copy.deepcopy(activity_points.values())
@@ -178,14 +203,54 @@ def profile_activity_points(activity_points):
     charting.show_charts()
 
 
+def get_osm_bus_stops(routes):
+    lines = []    
+    for route in routes:
+        lines.append(route.geometry.buffer(100))
+    routes_bounds = MultiPolygon(lines).bounds
+    south_west = utm.to_latlon(routes_bounds[0], routes_bounds[1], 37, 'M')
+    north_east = utm.to_latlon(routes_bounds[2], routes_bounds[3], 37, 'M')
+    bounding_box = (south_west[0],south_west[1],north_east[0],north_east[1])
+    osm = overpass.API()
+    # Get relevant OSM "nodes". For the sake of simplicity, "ways" are not considered here.
+    # Manual inspection showed that all "ways" in the area also contained at least one "node".
+    # Except Ubungo International Bus Terminal, however there is no activity point nearby.
+    response = osm.Get('(node[amenity=bus_station]'+str(bounding_box)+';'+
+                        'node[highway=bus_stop]'+str(bounding_box)+';);')
+    with open(DATA_PATH+'osm_bus_stops.geojson', 'w') as out:   
+        out.write(str(response))
+
+    osm_bus_stops = []
+    for feature in response['features']:
+        osm_bus_stops.append(BusStop(feature['geometry']['coordinates']))
+    return osm_bus_stops
+
+
+def extract_activity_pattern_around_bus_stops(bus_stops, activity_points, radius=250):
+    for bus_stop in bus_stops:
+        buffered_bus_stop = bus_stop.geometry.buffer(radius)
+        for point in activity_points.values():
+            if buffered_bus_stop.intersects(point.geometry):
+                bus_stop.nearby_activity_points.append(point)
+                
+
 def detect_bus_stops(activity_points, routes):
     enhance_activity_points(activity_points)
-    filtered_points = filter_by_distance(activity_points, routes)
-    print len(filtered_points)
-    write_geojson(filtered_points,'filtered.geojson')
-    # TODO
-    
 
+    interesting_activity_combinations = [
+                                            ('in_vehicle','still'),
+                                            ('in_vehicle','on_foot'),
+                                            ('in_vehicle','on_bicycle'),
+                                            ('still','in_vehicle'),
+                                            ('on_foot','in_vehicle'),
+                                            ('on_bicycle','in_vehicle')                                          
+                                        ]
+    filtered_points = filter_by_activity_combinations(activity_points, interesting_activity_combinations)
+    filtered_points = filter_by_distance(filtered_points, routes)
+    
+    write_geojson(filtered_points,'filtered_activity_points.geojson')
+    # TODO
+  
 
 activity_points_geojson = load_geojson('activity_points.geojson')
 activity_points = create_activity_points(activity_points_geojson)
@@ -193,8 +258,15 @@ activity_points = create_activity_points(activity_points_geojson)
 routes_geojson = load_geojson('routes.geojson')
 routes = create_routes(routes_geojson)
 
-profile_activity_points(activity_points)
+osm_bus_stops = get_osm_bus_stops(routes)
+extract_activity_pattern_around_bus_stops(osm_bus_stops, activity_points)
 
+for stop in osm_bus_stops:
+    if stop.nearby_activity_points:
+        for nearby in stop.nearby_activity_points:
+            print (nearby.previous_dominating_activity, nearby.current_dominating_activity, nearby.speed)
+        print
+#profile_activity_points(activity_points)
 #detect_bus_stops(activity_points,routes)
 
 
